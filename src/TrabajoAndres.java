17f02a677e0a49739d81e7d9bdaacec032e824cf
Noureddine Essbai <nes-sbai01@educarex.es>
2022-03-06 15:17:45 +0100

    1era versión

---
+++ src/TrabajoAndres.java
@@ -0,0 +1,20 @@
+
+public class TrabajoAndres {
+
+	public static void main(String[] args) {
+		int a= 7;
+		
+		int b= 5;
+		
+		int suma= a+b; 
+		int resta=a-b;
+		
+		System.out.println ("la suma de a y b es " + suma);
+		
+		System.out.println ("la resta de a y b es " + resta);
+		
+		System.out.println ("la multiplicación de a y b es " + a*b);
+
+	}
+
+}
